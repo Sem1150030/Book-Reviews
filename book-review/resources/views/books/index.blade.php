eb1862cf8842a5c1afb6f98b814230fb68b86372
Sem Star <s1150030@student.hsleiden.nl>
2025-07-04 13:52:46 +0200

    added Search bar and clear function using Query params

--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <h1 class="mb-10 text-2xl">Books</h1>
 
-<form></form>
+<form mtehode="GET" action="{{route('books.index')}}" class="mb-4 flex items-center space-x-2">
+    <input type="text" name="title" placeholder="Search by title"
+    value="{{request('title')}}" class="input">
+
+    <button type="submit" class="btn">Search</button>
+    <a href="{{route('books.index')}}" class="btn">Clear </a>
+</form>
 <ul>
     @forelse ($books as $book)
       <li class="mb-4">
